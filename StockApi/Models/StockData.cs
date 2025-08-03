b18d8e2dd3fd338a5c439c1cf315fe5cfd8253de
Aliaksei Nevelski <anev84@gmail.com>
2025-08-03 12:04:31 +0200

    modifi StockData from class to record

--- StockApi/Models/StockData.cs
+++ StockApi/Models/StockData.cs
@@ -2,27 +2,14 @@
 
 namespace StockApi.Models
 {
-    public class StockData
-    {
-        [JsonPropertyName("ticker")]
-        public required string Ticker { get; set; }
+    public record StockData(
+     string Ticker,
+     DateTime Date,
+     decimal Open,
+     decimal Close,
+     decimal High,
+     decimal Low,
+     long Volume
+    );
 
-        [JsonPropertyName("date")]
-        public DateTime Date { get; set; }
-
-        [JsonPropertyName("open")]
-        public decimal Open { get; set; }
-
-        [JsonPropertyName("close")]
-        public decimal Close { get; set; }
-
-        [JsonPropertyName("high")]
-        public decimal High { get; set; }
-
-        [JsonPropertyName("low")]
-        public decimal Low { get; set; }
-
-        [JsonPropertyName("volume")]
-        public long Volume { get; set; }
-    }
 }
